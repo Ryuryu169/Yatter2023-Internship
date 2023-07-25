55bef0ed18a199759ebb370ae8b627b8a28763db
Rewekana <ryusin916@gmail.com>
2023-07-25 17:40:39 +0900

    Commit 2023-07-25

---
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
@@ -0,0 +1,41 @@
+package com.dmm.bootcamp.yatter2023.ui.timeline
+
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.dmm.bootcamp.yatter2023.ui.theme.Yatter2023Theme
+import com.dmm.bootcamp.yatter2023.ui.timeline.bindingmodel.StatusBindingModel
+
+@Composable
+fun PublicTimelineTemplate(
+    statusList: List<StatusBindingModel>,
+    isLoading: Boolean,
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+) {
+
+}
+
+@Preview
+@Composable
+private fun PublicTimelineTemplatePreview() {
+    Yatter2023Theme {
+        Surface {
+            PublicTimelineTemplate(
+                statusList = listOf(
+                    StatusBindingModel(
+                        id = "id",
+                        displayName = "display name",
+                        username = "username",
+                        avatar = null,
+                        content = "preview content",
+                        attachmentMediaList = listOf()
+                    )
+                ),
+                isLoading = false,
+                isRefreshing = false,
+                onRefresh = {},
+            )
+        }
+    }
+}
